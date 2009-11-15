6cae9ea6bca0eb0c17edef66d7fed280869b805e
Thiago R. Colucci <thiagorcolucci@agilbits.com.br>
2009-11-14 22:11:50 -0200

    Criando new subject controller

--- Fase 5/nerd/config/routes.rb
+++ Fase 5/nerd/config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :subjects, :only => [:index]
+  map.resources :subjects, :only => [:index, :new]
 
   map.root :controller => "subjects", :action => "index"
 end
